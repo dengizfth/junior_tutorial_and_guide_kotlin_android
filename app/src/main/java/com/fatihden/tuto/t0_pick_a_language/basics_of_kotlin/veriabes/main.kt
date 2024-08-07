1d97edb8d9f3e34b651035e7e7f20e66d1f05232
Fatih Dengiz <dengizfth@gmail.com>
2024-08-07 21:21:57 +0300

    Basic of kotlin created files edit

--- app/src/main/java/com/fatihden/tuto/t0_pick_a_language/basics_of_kotlin/veriabes/main.kt
+++ app/src/main/java/com/fatihden/tuto/t0_pick_a_language/basics_of_kotlin/veriabes/main.kt
@@ -4,7 +4,7 @@ package com.fatihden.tuto.t0_pick_a_language.basics_of_kotlin.veriabes
 
 /*
 - **Variables:**
-  - `val` for immutable variables.
+  - `val` for immutable variables.-non-mutable
   - `var` for mutable variables.
 
 * */
